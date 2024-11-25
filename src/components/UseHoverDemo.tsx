7545ad69f805e9d2884e6116b2bbc980b9d386a5
Oleg Arbuzov <oleg.arbuzov@rt.ru>
2024-11-25 21:09:00 +0300

    feat: добавляет типизацию useHover useHoverDemo

---
+++ src/components/UseHoverDemo.tsx
@@ -0,0 +1,12 @@
+import * as React from 'react';
+import { useHover } from '../hooks/useHover';
+
+export function UseHoverDemo() {
+  const { hovered, ref } = useHover<HTMLButtonElement>();
+
+  return (
+      <button ref={ref}>
+        {hovered ? 'На меня навели мышку' : 'Наведи мышкой на меня'}
+      </button>
+  );
+}
